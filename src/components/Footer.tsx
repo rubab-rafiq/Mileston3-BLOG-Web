a90f9f2c18b720183eb25538c5a3d3034214ee0e
rubab-rafiq <rubabrafique05@gmail.com>
2025-01-04 02:57:10 -0800

    fixed error

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,24 @@
+
 import Link from "next/link";
 import React from "react";
-  import ContactSection from "./FooterContactForm";
+import ContactSection from "./FooterContactForm";
 import FooterLearn from "./FooterLearn";
 import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className=" w-full bg-black  mt-12">
+    <footer className="w-full bg-black mt-12">
       <section className="flex flex-col md:flex-row">
-         <FooterLearn />
-         <ContactSection />  
+        <FooterLearn />
+        <ContactSection />
       </section>
-      <section className=" px-6 xs:px-8 sm:px-12  lg:px-16 xl:px-24 2xl:px-32 py-2 flex flex-col sm:flex-row justify-between items-center gap-2 ">
-
-      <div className="flex justify-center items-center gap-x-2 text-sm text-light">
+      <section className="px-6 xs:px-8 sm:px-12 lg:px-16 xl:px-24 2xl:px-32 py-2 flex flex-col sm:flex-row justify-between items-center gap-2">
+        <div className="flex justify-center items-center gap-x-2 text-sm text-light">
           <p className="">
             Developed
-            <span>&nbsp;💖&nbsp;with&nbsp;</span>
+            <span>&nbsp;💖&nbsp;with&nbsp;</span>&quot;Next.js&quot;
           </p>
+
           <Link href={"https://nextjs.org/"} target="_blank">
             <Image src="/nextjs.svg" alt="next.js 14" width={24} height={24} />
           </Link>
@@ -26,21 +27,18 @@ export default function Footer() {
             <Image src="/sanity.svg" alt="sanity" width={24} height={24} />
           </Link>
         </div>
-      
+
         <div>
           <p className="text-xs text-light">
-            © 2024 <span className="text-light font-bold">&nbsp;AI</span>
+            © 2024{" "}
+            <span className="text-light font-bold">&nbsp;AI</span>
             <span className="text-accentDarkPrimary font-bold">
               BLOG&nbsp;
             </span>{" "}
             All rights reserved.
           </p>
         </div>
-
-
-
       </section>
-
     </footer>
   );
 }
